6b924ad2b4ca237c04c9cad32a51ba0e987717b1
Bruno Costa <bhcosta90@gmail.com>
2025-01-06 19:40:03 -0300

    wip

---
+++ tests/Unit/Factory/DateRangeFactoryTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use Package\Factory\DateRangeFactory;
+use Package\Factory\DateRangeFactoryInterface;
+use Package\ValueObject\DateRange;
+
+test('criando uma nova data range', function(){
+    $dateRange = ($objDateRange = new DateRangeFactory())->create('2021-01-01', '2021-01-31');
+
+    expect($objDateRange)->toBeInstanceOf(DateRangeFactoryInterface::class)
+        ->and($dateRange)->toBeInstanceOf(DateRange::class);
+});
